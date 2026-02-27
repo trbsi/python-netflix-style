9d58e2a91f1890bfb1e49e3fd9cf3baaf5208dd8
Dario <dario@mail.com>
2026-02-27 20:27:31 +0100

    add import_from_dump_task

--- src/media/tasks.py
+++ src/media/tasks.py
@@ -3,5 +3,10 @@ from django.core.management import call_command
 
 
 @shared_task
-def import_from_rss():
+def import_from_rss_task():
     call_command('import_rss_feed_command')
+
+
+@shared_task
+def import_from_dump_task():
+    call_command('import_dump_command')
