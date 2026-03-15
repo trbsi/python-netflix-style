18839468988ee3d51085753a4858a65f70332a38
Dario <dario@mail.com>
2026-03-15 12:00:14 +0100

    18 plus popup

--- static/js/core/core.js
+++ static/js/core/core.js
@@ -1,3 +1,20 @@
 function getCsrfToken() {
     return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 }
+
+$(document).ready(function () {
+    if (localStorage.getItem("peachka18") !== "true") {
+        var ageModal = new bootstrap.Modal(document.getElementById('ageModal'));
+        ageModal.show();
+    }
+
+    $("#enterSite").click(function () {
+        localStorage.setItem("peachka18", "true");
+        $("#ageModal").modal("hide");
+    });
+
+    $("#leaveSite").click(function () {
+        window.location.href = "https://google.com";
+    });
+});
+
