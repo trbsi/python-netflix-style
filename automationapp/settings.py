40698f5ff93f600e686e5bac97f1f8cd7334106d
Julye <julyemashic@mail.com>
2026-03-16 08:44:41 +0100

    force ssl

--- automationapp/settings.py
+++ automationapp/settings.py
@@ -37,6 +37,7 @@ DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS').split(',')
+SECURE_SSL_REDIRECT = True
 
 # Application definition
 
