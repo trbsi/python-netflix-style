d1b97b8554fe0fe8fec58601912555d9e292e18a
Julye <julyemashic@mail.com>
2026-03-19 09:50:32 +0100

    update command message

--- src/sitemap/management/commands/generate_sitemap_command.py
+++ src/sitemap/management/commands/generate_sitemap_command.py
@@ -17,16 +17,22 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        is_full = options["full"]
         start = time.time()
 
         self.info('Starting to generate sitemaps')
-        
+
         service = GenerateSitemapService()
-        service.generate_sitemap(options["full"])
+        service.generate_sitemap(is_full)
 
         end = time.time()
         minutes = (end - start) / 60
-        message = f"GenerateSitemapCommand. Execution time: {minutes:.2f} minutes."
+        
+        message = f"GenerateSitemapCommand. Execution time: {minutes:.2f} minutes. "
+        if is_full:
+            message = message + "Full sitemaps generated."
+        else:
+            message = message + "Incremental sitemaps generated."
 
         print(message)
         bugsnag.notify(Exception(message))
