7f01584f917ab21b7ec5c3094929c3290d11d390
Julye <julyemashic@mail.com>
2026-03-24 08:39:16 +0100

    Add image effect

--- static/js/core/core.js
+++ static/js/core/core.js
@@ -2,6 +2,7 @@ function getCsrfToken() {
     return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 }
 
+// 18+ popup
 $(document).ready(function () {
     if (localStorage.getItem("peachka18") !== "true") {
         var ageModal = new bootstrap.Modal(document.getElementById('ageModal'));
@@ -18,3 +19,14 @@ $(document).ready(function () {
     });
 });
 
+// thumbnail effect variant
+(function () {
+  let variant = localStorage.getItem("ab_variant");
+
+  if (!variant) {
+    variant = Math.random() < 0.5 ? "A" : "B";
+    localStorage.setItem("ab_variant", variant);
+  }
+
+  document.documentElement.setAttribute("data-variant", variant);
+})();
