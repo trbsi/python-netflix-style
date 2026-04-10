aabf20a37ed906d6e182d9f158e2c54e1fcad084
Julye <julyemashic@mail.com>
2026-04-10 08:15:54 +0200

    Make privatniadmin root

--- src/myadmin/urls.py
+++ src/myadmin/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('commands', views.commands, name='admin.commands'),
+    path('', views.commands, name='admin.commands'),
     path('trigger/import-partial', views.trigger_partial_import, name='admin.trigger_import_partial'),
     path('trigger/frontend', views.trigger_generate_frontend, name='admin.trigger_generate_frontend'),
     path('trigger/sitemap-partial', views.trigger_sitemap_partial, name='admin.trigger_sitemap_partial'),
