7eceed5b3f91221b12e5915e0de22ea28a416551
Julye <julyemashic@mail.com>
2026-04-13 09:42:32 +0200

    limit pagination

--- src/media/services/all_videos/all_videos_service.py
+++ src/media/services/all_videos/all_videos_service.py
@@ -3,12 +3,18 @@ from django.core.paginator import Paginator, Page
 from src.media.models import VideoItem
 
 
+class LargeTablePaginator(Paginator):
+    @property
+    def count(self):
+        return 10000000
+
+
 class AllVideosService:
     PER_PAGE = 25
 
     def get_all_videos(self, current_page: int) -> Page:
-        videos = VideoItem.objects.order_by('id')
-        paginator = Paginator(object_list=videos, per_page=self.PER_PAGE)
+        videos = VideoItem.objects.order_by('-id')
+        paginator = LargeTablePaginator(object_list=videos, per_page=self.PER_PAGE)
         page = paginator.page(current_page)
 
         return page
