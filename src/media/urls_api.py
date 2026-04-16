dd9f3f92d4d157576316fff11c86ad2c6bd11e0d
Dario <dario@mail.com>
2026-04-16 15:33:21 +0200

    return back removed routes

--- src/media/urls_api.py
+++ src/media/urls_api.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('categories/search', views.categories_search_api, name='media.api.categories_search'),
     path('search', views.search_videos_api, name='media.api.search'),
+    path('update-title', views.update_title_rewritten_api),
+    path('get-title', views.get_title_rewritten_api),
 ]
