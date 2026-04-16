8e8919da87edb9239febfd61d3212ac5d66a5964
Dario <dario@mail.com>
2026-04-16 17:50:48 +0200

    add gradual rollout to cateogires

--- src/media/services/categories/search_by_category_service.py
+++ src/media/services/categories/search_by_category_service.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 
+from src.core.utils import GRADUAL_ROLLOUT
 from src.media.models import VideoItem, VideoCategory, VideoCategoryPivot
 
 
@@ -46,7 +47,7 @@ class SearchByCategoryService:
             VideoItem.objects
             .filter(categories_relation=category)
             .order_by('-id')
-            .distinct()
+            .distinct()[:GRADUAL_ROLLOUT]
         )
 
         paginator = Paginator(queryset, self.PAGE_SIZE)
