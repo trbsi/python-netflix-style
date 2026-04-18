bfc61e5ff12382d4a3177871898744a01d82a0b4
Dario <dario@mail.com>
2026-04-18 18:33:50 +0200

    Add install app

---
+++ static/js/core/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "app-cache-v1";
+const urlsToCache = [
+    "/",
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('{% static 'js/core/service-worker.js' %}')
+        .then(function (reg) {
+            console.log("Service worker registered.", reg);
+        });
+}
