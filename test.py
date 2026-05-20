232a82a5137f6ddc088825379254bc16f1741878
Julye <julyemashic@mail.com>
2026-05-20 08:04:23 +0200

    remote test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('canonical_tags.json', 'r') as f:
-    json = json.load(f)
-
-for key, data in json.items():
-    for canonical, tags in data.items():
-        print(canonical)
