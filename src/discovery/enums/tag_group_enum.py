fb984ec7b21c0af4980a1593685623f1ae307c19
Julye <julyemashic@mail.com>
2026-06-02 20:15:40 +0200

    update tags

--- src/discovery/enums/tag_group_enum.py
+++ src/discovery/enums/tag_group_enum.py
@@ -2,21 +2,21 @@ from enum import Enum
 
 
 class TagGroupEnum(Enum):
-    roles = 4.5
-    kinks = 4
+    role = 4.5
+    kink = 4
     setting = 3.5
     appearance = 3
-    positions = 2.5
-    acts = 2
-    categories = 1.5
+    position = 2.5
+    act = 2
+    category = 1.5
     """
-    roles           → who,
-    kinks           → relational structure,
+    role           → who,
+    kink           → relational structure,
     setting         → where,
     appearance      → what they are,
-    positions       → sex positions,
-    acts            → what happens,
-    categories      → weak fallback label
+    position       → sex positions,
+    act            → what happens,
+    category        → weak fallback label
     """
 
     @staticmethod
